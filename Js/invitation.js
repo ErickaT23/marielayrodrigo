9bfe2a9ae78cf572dd19c242387e7c0f100521f1
Ericka Tuchan <125705781+ErickaT23@users.noreply.github.com>
2024-08-02 18:49:42 -0600

    background position

--- Js/invitation.js
+++ Js/invitation.js
@@ -1,10 +1,14 @@
 function showInvitation() {
+    const body = document.body;
     const overlay = document.getElementById('overlay');
     const invitation = document.getElementById('invitation');
+
     overlay.classList.add('fade-out');
     setTimeout(() => {
         overlay.classList.add('hidden');
+        body.classList.remove('bg-cover');
+        body.classList.add('bg-contain');
         invitation.classList.remove('hidden');
         invitation.classList.add('show');
-    }, 1000); // La duración debe coincidir con la transición de la opacidad
+    }, 1000);
 }
